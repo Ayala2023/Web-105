ce61250647cae8ac3c474a6982b4da8ec689daef
Ayala2023 <aayala1992@students.mchenry.edu>
2024-04-10 11:32:23 -0500

    April 10

---
+++ Javascript/script.js
@@ -0,0 +1,8 @@
+/* https://www.w3schools.com/js/tryit.asp?filename=tryjs_intro_lightbulb */
+
+
+
+/* change the image */
+function change(picture) { //change takes picture, picture is a image addres
+    document.getElementById("large").src = picture; 
+}
